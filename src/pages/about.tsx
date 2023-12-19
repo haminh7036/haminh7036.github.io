b41c701ecc27e7a83f7f5223b01734f16abbe050
Minh Ha <66556512+haminh7036@users.noreply.github.com>
2023-12-19 14:06:51 +0700

    image test

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import robinImage from '../../public/robin.jpg';
+
 export default function About() {
-    return <div>About</div>
+    return <div>
+        About
+        <br />
+        <Image alt="" src={robinImage} />
+    </div>
 }
