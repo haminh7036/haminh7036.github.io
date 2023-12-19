e16fae60c03f9d8b87c5bb5768455d28577775bb
Minh Ha <66556512+haminh7036@users.noreply.github.com>
2023-12-19 11:22:08 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     // images: {
     //     loader: 'akamai',
     //     path: '',
-    //   },
+        
+    // },
     // assetPrefix: './',
 }
 
