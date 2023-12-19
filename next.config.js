c1c770c2c316eb626b495a8a8f49b5d450f8d8e3
Minh Ha <66556512+haminh7036@users.noreply.github.com>
2023-12-19 14:09:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
 
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
@@ -12,9 +12,9 @@ const nextConfig = {
 
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
-    // images: {
-    //     unoptimized: true
-    // }
+    images: {
+        unoptimized: true
+    }
 }
 
 module.exports = nextConfig
